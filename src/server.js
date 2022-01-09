e185554435dac83232872c46ebd2e7a701c0981b
IsaiasMCC <isaisvhsyw@gmail.com>
2022-01-08 23:15:36 -0400

    deploy gaa

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 class Server {
     constructor(){
         this.app = express();
-        this.port = 8081;
+        this.port = process.env.PORT || 5050;
 
         this.middlewares();
       
@@ -12,7 +12,7 @@ class Server {
     middlewares(){
         this.app.use( express.static('public'));
 
-        
+
         this.app.use(express.json());
     }
     
